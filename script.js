750a6524ec6582eeb49d8d36e5a6648544db98db
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-07-18 02:16:19 -0400

    Fixed some previous issues with work experience, also took out the dissolve effect. Seemed pretty useless.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-$(window).on('beforeunload', function() {
-    $(this).scrollTop(0);
-})
+// $(window).on('beforeunload', function() {
+//     $(this).scrollTop(0);
+// })
 
 lastY = $(this).scrollTop();
 
@@ -50,43 +50,10 @@ $(document)
                 }
             });
 
-        $(window).bind('scroll mousewheel', function(event) {
-            // Masthead Parralax Effect
-            if ($('.masthead').isInViewport()) {
-                var currY = $(this).scrollTop();
-                var pos = $('.masthead').css("background-position").split(" ");
-
-                // At the top
-                if (currY === 0) {
-                    $('.masthead').css("background-position", `${pos[0]} 0px`);
-                } else {
-                    if (currY > lastY) {
-                        // Down
-                        $('.masthead').css("background-position", `${pos[0]} ${parseFloat(pos[1]) - 0.1}px`);
-                    } else if (currY < lastY) {
-                        // Up
-                        $('.masthead').css("background-position", `${pos[0]} ${parseFloat(pos[1]) + 0.1}px`);
-
-                    }
-                }
-            }
-
-            // In-view fade-in transition
-            $.each($('.main-section'), function() {
-                var current = $(this);
-                if (current.isInViewport()) {
-                    current.addClass("in-view");
-                } else {
-                    current.removeClass("in-view");
-                }
-            })
-
-            lastY = currY;
-
-        });
-
         function smooth_scroll(e) {
             var hash = this.hash;
+            $(".ui.sidebar").sidebar("hide");
+
             if (this.hash !== "") {
                 $("html body").animate({
                     scrollTop: $(hash).offset().top + 1
@@ -107,6 +74,7 @@ $(document)
         $("#follow-menu a").on("click", smooth_scroll);
         $("#main-menu a").on("click", smooth_scroll);
         $("#about-me a").on("click", smooth_scroll);
+        $('.ui.sidebar a').on("click", smooth_scroll);
 
         $('#education').visibility(vis_obj_factory("#education"));
         $('#work').visibility(vis_obj_factory("#work"));
